1ba0ceb2e6d4300ddfe34bc1d4b2af694acfe8ee
Suraj Ganguly <84933503+Suraj18893@users.noreply.github.com>
2021-06-08 23:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var manuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+// var menu = document.getElementById("menu");
+sideNav.style.right = "-250px"
+menuBtn.onclick = function(){
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0px"
+    }
+    else{
+        sideNav.style.right ="-250px"
+    }
+}
